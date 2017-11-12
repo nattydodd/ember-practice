7ec7fd0c09297938c7a30e8ab66bc474e02b923e
Natalie Dodd <nataliedodd89@gmail.com>
2017-11-12 16:33:11 +0100

    service to change progress now working'

--- app/components/days/days-door.js
+++ app/components/days/days-door.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   daysService: Ember.inject.service('days-service'),
+  lastDoor: false,
+
 
   calculatePercentage(id) {
     let percentage = id / 25 * 100;
@@ -11,8 +13,13 @@ export default Ember.Component.extend({
   actions: {
 
     toggleOpen(id) {
-      console.log('open');
-      this.get('daysService').setProgress(calculatePercentage(id));
+      if ($(`#door${id}`).hasClass("days__list__item__door__inner--open")) {
+        this.get('daysService').setProgress(this.calculatePercentage(id - 1));
+        $(`#door${id}`).toggleClass("days__list__item__door__inner--open");
+      } else {
+        this.get('daysService').setProgress(this.calculatePercentage(id));
+        $(`#door${id}`).toggleClass("days__list__item__door__inner--open");
+      }
     }
 
   }
--- app/components/days/days-progressbar.js
+++ app/components/days/days-progressbar.js
@@ -4,7 +4,13 @@ export default Ember.Component.extend({
   daysService: Ember.inject.service('days-service'),
 
   didInsertElement() {
-    this.set('progress', this.get('daysService').getProgress);
-  }
+    let progress = this.get('daysService').getProgress();
+    this.set('progress', progress);
+  },
+
+  progressObserver: Ember.observer('daysService.progress', function() {
+    let progress = this.get('daysService').getProgress();
+    this.set('progress', progress);
+  })
 
 });
